4e7949d992a2fcca0a37785307aef1b3111562df
mofrim <mofrim@posteo.de>
2024-12-06 12:46:27 +0100

    fix quots in quots

--- src/tokenization/tok_lvl1_get_next_helpers2.c
+++ src/tokenization/tok_lvl1_get_next_helpers2.c
@@ -6,7 +6,7 @@
 /*   By: fmaurer <fmaurer42@posteo.de>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/24 23:47:57 by fmaurer           #+#    #+#             */
-/*   Updated: 2024/12/06 00:18:06 by fmaurer          ###   ########.fr       */
+/*   Updated: 2024/12/06 12:45:21 by fmaurer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	get_tok_quot(t_token *tok, t_cmdline *cl, int *tok_found)
 {
 	if (!*tok_found)
 	{
-		if (cl->input[cl->pos] == '"')
+		if (cl->input[cl->pos] == '"' && !cl->squot_flag)
 		{
 			tok->type = TOK_DQUOT;
 			tok->value = ft_strdup("\"");
@@ -25,7 +25,7 @@ void	get_tok_quot(t_token *tok, t_cmdline *cl, int *tok_found)
 			*tok_found = 1;
 			cl->dquot_flag = !cl->dquot_flag;
 		}
-		if (cl->input[cl->pos] == '\'')
+		if (cl->input[cl->pos] == '\'' && !cl->dquot_flag)
 		{
 			tok->type = TOK_SQUOT;
 			tok->value = ft_strdup("\'");
@@ -80,7 +80,7 @@ void	get_tok_squotword(t_token *tok, t_cmdline *cl, int *tok_found)
 				cl->pos++;
 			length = cl->pos - start;
 			word = malloc(length + 1);
-			nullcheck(tok->value, "get_tok_squotword()");
+			nullcheck(word, "get_tok_squotword()");
 			ft_strlcpy(word, &cl->input[start], length + 1);
 			tok->value = word;
 			tok->type = TOK_WORD;
