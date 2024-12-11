9287c41784e09e8f00253f8e46319a98962e64ee
Elpah <obengelpachris@gmail.com>
2024-12-11 12:59:50 +0100

    implemented env

--- src/builtins/bltin_env.c
+++ src/builtins/bltin_env.c
@@ -3,16 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   bltin_env.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: eobeng <eobeng@student.42.fr>              +#+  +:+       +#+        */
+/*   By: elpah <elpah@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/05 10:13:46 by fmaurer           #+#    #+#             */
-/*   Updated: 2024/12/06 17:13:29 by eobeng           ###   ########.fr       */
+/*   Updated: 2024/12/11 12:58:40 by elpah            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+void	print_env_vars(t_envlst *el)
+{
+	while (el)
+	{
+		if (el->value)
+			ft_printf("%s=\"%s\"\n", el->name, el->value);
+		el = el->next;
+	}
+    return ;
+}
+
 int	env(t_envlst *env)
 {
-    print_envlst(env);
+    print_env_vars(env);
     return (0);
 }
