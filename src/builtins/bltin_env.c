83aa8b11b806b3199544bcf0479e7ef5643b72e3
Elpah <obengelpachris@gmail.com>
2024-12-16 11:01:43 +0100

    implemented env function

--- src/builtins/bltin_env.c
+++ src/builtins/bltin_env.c
@@ -3,19 +3,38 @@
 /*                                                        :::      ::::::::   */
 /*   bltin_env.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fmaurer <fmaurer42@posteo.de>              +#+  +:+       +#+        */
+/*   By: elpah <elpah@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/05 10:13:46 by fmaurer           #+#    #+#             */
-/*   Updated: 2024/12/08 01:05:09 by fmaurer          ###   ########.fr       */
+/*   Updated: 2024/12/16 10:51:47 by elpah            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	bltin_env(char **arg, t_envlst **el)
+void	print_env_vars(t_envlst *el)
 {
-	(void)arg;
-	(void)el;
-	ft_printf(RED "<< DEBUG >> bltin_env not yet implemented!\n" RST);
+	while (el)
+	{
+		if (el->value)
+			ft_printf("%s=\"%s\"\n", el->name, el->value);
+		el = el->next;
+	}
+	return ;
+}
+
+int	bltin_env(t_envlst *env, char **str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	if (i > 1)
+	{
+		ft_printf("minishell: env: %s: No such file or directory\n", str[1]);
+		return (1);
+	}
+	print_env_vars(env);
 	return (0);
 }
