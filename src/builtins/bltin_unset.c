aa54fc723cba2d2aa6cdf62e2f350c296a5132c2
Elpah <obengelpachris@gmail.com>
2024-12-16 10:39:06 +0100

    test

--- src/builtins/bltin_unset.c
+++ src/builtins/bltin_unset.c
@@ -3,19 +3,67 @@
 /*                                                        :::      ::::::::   */
 /*   bltin_unset.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fmaurer <fmaurer42@posteo.de>              +#+  +:+       +#+        */
+/*   By: elpah <elpah@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/05 10:14:32 by fmaurer           #+#    #+#             */
-/*   Updated: 2024/12/08 01:06:49 by fmaurer          ###   ########.fr       */
+/*   Updated: 2024/12/16 10:37:45 by elpah            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	bltin_unset(char **arg, t_envlst **el)
+
+void unset_free(t_envlst *ptr)
 {
-	(void)arg;
-	(void)el;
-	ft_printf(RED "<< DEBUG >> bltin_unset not yet implemented!\n" RST);
-	return (0);
+	if (!ptr)
+		return;
+	free(ptr->name);
+	free(ptr->value);
+	free(ptr);
+}
+
+int check(char **key)
+{
+	if (!key[1])
+	{
+		ft_printf("-minishell: unset: not enough arguments\n");
+		return (0);
+	}
+	if (key[1][0] == '=' || key[1][0] == '?' || key[1][0] == '-')
+	{
+		ft_printf("-minishell: unset: '%s': not a valid identifier\n", key[1]);
+		return (0);
+	}
+	return (1);
+}
+
+int bltin_unset(t_envlst **env, char **str)
+{
+	t_envlst *ptr;
+	t_envlst *prev;
+	t_envlst *ptr2;
+
+	if (!env || !*env || !check(str))
+		return (0);
+
+	ptr = *env;
+	if (!ft_strcmp(ptr->name, str[1]))
+	{
+		*env = ptr->next;
+		unset_free(ptr);
+		return (0);
+	}
+	prev = ptr;
+	while (ptr && ptr->next)
+	{
+		if (!ft_strcmp(ptr->next->name, str[1]))
+		{
+			ptr2 = ptr->next;
+			ptr->next = ptr->next->next;
+			unset_free(ptr2);
+			return (0);
+		}
+		ptr = ptr->next;
+	}
+	return 		(0);
 }
