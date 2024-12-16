da7f357ed59ad673515548e5bc8657a85e787ef0
Elpah <obengelpachris@gmail.com>
2024-12-16 10:28:53 +0100

    implemented echo function

--- src/builtins/bltin_echo.c
+++ src/builtins/bltin_echo.c
@@ -3,19 +3,51 @@
 /*                                                        :::      ::::::::   */
 /*   bltin_echo.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fmaurer <fmaurer42@posteo.de>              +#+  +:+       +#+        */
+/*   By: elpah <elpah@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/05 10:13:37 by fmaurer           #+#    #+#             */
-/*   Updated: 2024/12/08 01:04:39 by fmaurer          ###   ########.fr       */
+/*   Updated: 2024/12/16 10:28:32 by elpah            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	bltin_echo(char **arg, t_envlst **el)
+int	array_length(char **str)
 {
-	(void)arg;
-	(void)el;
-	ft_printf(RED "<< DEBUG >> bltin_echo not yet implemented!\n" RST);
+	int	i;
+
+	if (!str || !str[0])
+		return (0);
+	i = 0;
+	while (str[i] != NULL)
+		i++;
+	return (i);
+}
+
+int	bltin_echo(char **s)
+{
+	int		i;
+	char	**str;
+
+	i = 0;
+	if (array_length(s) == 1)
+	{
+		ft_printf("\n");
+		return (0);
+	}
+	str = &s[1];
+	if (str)
+	{
+		if (!ft_strncmp(str[i], "-n", 2) && ft_strlen(str[i]) == 2)
+			str = &str[1];
+		while (str[i])
+		{
+			ft_printf("%s", str[i++]);
+			if (str[i])
+				ft_printf(" ");
+		}
+	}
+	if (!(!ft_strncmp(s[1], "-n", 2) && ft_strlen(s[1]) == 2))
+		ft_printf("\n");
 	return (0);
 }
