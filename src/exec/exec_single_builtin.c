a332b02b1f3647e502034b348d80e179e5eea8b8
mofrim <mofrim@posteo.de>
2024-12-22 21:08:52 +0100

    rewrite exec_single_builtin w. generalized func

--- src/exec/exec_single_builtin.c
+++ src/exec/exec_single_builtin.c
@@ -6,38 +6,42 @@
 /*   By: elpah <elpah@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/07 23:15:39 by fmaurer           #+#    #+#             */
-/*   Updated: 2024/12/20 13:39:16 by fmaurer          ###   ########.fr       */
+/*   Updated: 2024/12/22 21:07:59 by fmaurer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static int	exec_single_builtin(t_cmdlst *cl, t_envlst **el);
-
-/* Execute the single builtin command. Also opens possible redirection files
- * beforehand.
+/**
+ * Generalized buitlin execution function.
  *
- * We are not allowed to use WIFEXITED and WEXITSTATUS as they are macros. But
- * the calculation needed for extracting the real exit status from that recorded
- * by waitpid is simply shifting the bits to the right by 8. */
-// int	exec_single_builtin_cmd(t_cmdlst *cmdl, t_envlst **el)
-// {
-// 	int	exit_status;
-// 	int	cpid;
-//
-// 	exit_status = 0;
-// 	cpid = fork();
-// 	if (cpid == -1)
-// 		return (errno);
-// 	if (cpid == 0)
-// 	{
-// 		if (open_redir_files(cmdl->input_file, cmdl->outfiles))
-// 			exit(errno);
-// 		exit(exec_single_builtin(cmdl, el));
-// 	}
-// 	waitpid(cpid, &exit_status, 0);
-// 	return (exit_status >> 8);
-// }
+ * Params: cmdlst, envlst, function handling pre-output stuff that changes data
+ * which should be available to main process, function handling output stuff
+ * which gets executed in a child process with all redirects active.
+ */
+int	exec_single_builtin(t_cmdlst *cl, t_envlst **el, \
+		int (*bltin_preout)(t_cmdlst *, t_envlst **), \
+		int (*bltin_out)(t_cmdlst *, t_envlst **))
+{
+	int	exit_status;
+	int	cpid;
+
+	exit_status = 0;
+	if (bltin_preout)
+		exit_status = bltin_preout(cl, el);
+	cpid = fork();
+	if (cpid == -1)
+		return (errno);
+	if (cpid == 0)
+	{
+		if (open_redir_files(cl->input_file, cl->outfiles))
+			exit(errno);
+		bltin_out(cl, el);
+		exit(exit_status);
+	}
+	waitpid(cpid, &exit_status, 0);
+	return (exit_status >> 8);
+}
 
 /* Finally find the correct builtin function, call it & return their
  * exit_status. */
@@ -47,18 +51,43 @@ int	exec_single_builtin_cmd(t_cmdlst *cl, t_envlst **el)
 
 	exit_status = 0;
 	if (!strcmp(cl->cmd, "echo"))
-		exit_status = bltin_echo(cl->args);
+		exit_status = bltin_echo(cl);
 	if (!strcmp(cl->cmd, "cd"))
 		exit_status = bltin_cd(cl->args, el);
 	if (!strcmp(cl->cmd, "pwd"))
 		exit_status = bltin_pwd();
 	if (!strcmp(cl->cmd, "export"))
-		exit_status= bltin_export(cl, el);
+		exit_status = exec_single_builtin(cl, el, bltin_export_preout, \
+				bltin_export_out);
 	if (!strcmp(cl->cmd, "unset"))
 		exit_status = bltin_unset(el, cl->args);
 	if (!strcmp(cl->cmd, "env"))
-		exit_status = bltin_env(cl, *el);
+		exit_status = exec_single_builtin(cl, el, NULL, bltin_env);
 	if (!strcmp(cl->cmd, "exit"))
 		exit_status = bltin_exit();
 	return (exit_status);
 }
+
+/*
+ * Execution skeleton for builtins:
+ *
+ * int	exec_single_builtin_cmd(t_cmdlst *cmdl, t_envlst **el)
+ * {
+ * 	int	exit_status;
+ * 	int	cpid;
+ * 
+ * 	exit_status = 0;
+ * 	cpid = fork();
+ * 	if (cpid == -1)
+ * 		return (errno);
+ * 	if (cpid == 0)
+ * 	{
+ * 		if (open_redir_files(cmdl->input_file, cmdl->outfiles))
+ * 			exit(errno);
+ * 		exit(exec_single_builtin(cmdl, el));
+ * 	}
+ * 	waitpid(cpid, &exit_status, 0);
+ * 	return (exit_status >> 8);
+ * }
+ *
+ */
