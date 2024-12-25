f56652e07d266da34e7e8cdb31486274070e3c7d
mofrim <mofrim@posteo.de>
2024-12-25 22:45:14 +0100

    make tok_tokerr_int use dprintf to stderr

--- src/tokenization/tok_tokerr_int.c
+++ src/tokenization/tok_tokerr_int.c
@@ -6,7 +6,7 @@
 /*   By: fmaurer <fmaurer42@posteo.de>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/25 17:26:43 by fmaurer           #+#    #+#             */
-/*   Updated: 2024/12/25 17:36:03 by fmaurer          ###   ########.fr       */
+/*   Updated: 2024/12/25 22:44:49 by fmaurer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,31 +25,37 @@ int	token_error_int(t_tokerr te, char *tok)
 void	token_error_int_part1(t_tokerr te, char *tok)
 {
 	if (te == TOKERR_NL)
-		ft_printf("Syntax error near unexpected token: \'\\n\'\n");
+		ft_dprintf(2, "minishell: Syntax error near unexpected token: "\
+				"\'\\n\'\n");
 	if (te == TOKERR_PIP)
-		ft_printf("Syntax error near unexpected token: \'|\'\n");
+		ft_dprintf(2, "minishell: Syntax error near unexpected token: \'|\'\n");
 	if (te == TOKERR_RIN)
-		ft_printf("Syntax error near unexpected token: \'<\'\n");
+		ft_dprintf(2, "minishell: Syntax error near unexpected token: \'<\'\n");
 	if (te == TOKERR_ROUT)
-		ft_printf("Syntax error near unexpected token: \'>\'\n");
+		ft_dprintf(2, "minishell: Syntax error near unexpected token: \'>\'\n");
 	if (te == TOKERR_ROUT2)
-		ft_printf("Syntax error near unexpected token: \'&>\'\n");
+		ft_dprintf(2, "minishell: Syntax error near unexpected token: " \
+				"\'&>\'\n");
 	if (te == TOKERR_ROUT3)
-		ft_printf("Syntax error near unexpected token: \'>&\'\n");
+		ft_dprintf(2, "minishell: Syntax error near unexpected token: " \
+				"\'>&\'\n");
 	if (te == TOKERR_ROUTA)
-		ft_printf("Syntax error near unexpected token: \'>>\'\n");
+		ft_dprintf(2, "minishell: Syntax error near unexpected token: " \
+				"\'>>\'\n");
 	if (te == TOKERR_ROUTA2)
-		ft_printf("Syntax error near unexpected token: \'&>>\'\n");
-	if (te == TOKERR_HERE)
-		ft_printf("Syntax error near unexpected token: \'<<\'\n");
-	if (te == TOKERR_AND)
-		ft_printf("Syntax error near unexpected token: \'&\'\n");
-	if (te == TOKERR_REDIR)
-		ft_printf("minishell: %s: ambiguous redirect\n", tok);
+		ft_dprintf(2, "minishell: Syntax error near unexpected token: " \
+				"\'&>>\'\n");
 }
 
 void	token_error_int_part2(t_tokerr te, char *tok)
 {
+	if (te == TOKERR_HERE)
+		ft_dprintf(2, "minishell: Syntax error near unexpected token: "\
+				"\'<<\'\n");
+	if (te == TOKERR_AND)
+		ft_dprintf(2, "minishell: Syntax error near unexpected token: \'&\'\n");
+	if (te == TOKERR_REDIR)
+		ft_dprintf(2, "minishell: %s: ambiguous redirect\n", tok);
 	if (te == TOKERR_FDFROM)
 		ft_dprintf(2, "minishell: Syntax error near unexpected token:"\
 				" \'\%s\'\n", tok);
