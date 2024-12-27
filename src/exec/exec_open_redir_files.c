798bc6e20f54ac6d341fc0a0d104a83d4fcb6d86
mofrim <mofrim@posteo.de>
2024-12-27 18:34:24 +0100

    remove commented out code from exec_open_redir...c

--- src/exec/exec_open_redir_files.c
+++ src/exec/exec_open_redir_files.c
@@ -6,7 +6,7 @@
 /*   By: fmaurer <fmaurer42@posteo.de>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/08 16:11:12 by fmaurer           #+#    #+#             */
-/*   Updated: 2024/12/27 17:05:03 by fmaurer          ###   ########.fr       */
+/*   Updated: 2024/12/27 18:34:00 by fmaurer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,47 +55,3 @@ int	process_redirlst(t_redirlst *rdl)
 	}
 	return (rval);
 }
-
-/* Open & create all outfiles like bash does. */
-// static int	open_create_outfiles(t_redirlst *ofl, int append)
-// {
-// 	int	fd;
-//
-// 	while (ofl)
-// 	{
-// 		if (append)
-// 			fd = open(ofl->name, O_APPEND | O_CREAT | O_WRONLY, 0600);
-// 		else
-// 			fd = open(ofl->name, O_TRUNC | O_CREAT | O_WRONLY, 0600);
-// 		if (fd == -1)
-// 			return (-1);
-// 		if (ofl->next)
-// 			close(fd);
-//
-// 		ofl = ofl->next;
-// 	}
-// 	return (fd);
-// }
-
-// int	open_redir_files(char *infile, t_redirlst *ofl)
-// {
-// 	int	fd;
-//
-// 	if (ofl)
-// 	{
-// 		// fd = open_create_outfiles(ofl, append);
-// 		// if (fd == -1)
-// 		// 	return (errno);
-// 		// dup2(fd, STDOUT_FILENO);
-// 		// close(fd);
-// 	}
-// 	if (infile)
-// 	{
-// 		fd = open(infile, O_RDONLY);
-// 		if (fd == -1)
-// 			return (errno);
-// 		dup2(fd, STDIN_FILENO);
-// 		close(fd);
-// 	}
-// 	return (0);
-// }
