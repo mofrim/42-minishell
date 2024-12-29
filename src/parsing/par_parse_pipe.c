b959af75c3a0969c2e53e010d8f11cab54482331
mofrim <mofrim@posteo.de>
2024-12-29 12:32:12 +0100

    fix infinite loop in parse_pipe

--- src/parsing/par_parse_pipe.c
+++ src/parsing/par_parse_pipe.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   par_parsing_helpers1.c                             :+:      :+:    :+:   */
+/*   par_parse_pipe.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fmaurer <fmaurer42@posteo.de>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 11:29:40 by fmaurer           #+#    #+#             */
-/*   Updated: 2024/12/27 16:02:11 by fmaurer          ###   ########.fr       */
+/*   Updated: 2024/12/29 12:21:07 by fmaurer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,8 @@ void	parse_pipe(t_toklst **toklst, t_cmdlst **cmd, t_cmdlst **cur_cmd, \
 	if (*toklst && (*toklst)->next)
 	{
 		if ((*toklst)->token->type == TOK_PIP && \
-				(*toklst)->next->token->type == TOK_CMD)
+				((*toklst)->next->token->type == TOK_CMD || \
+				 (*toklst)->next->token->type == TOK_BLTIN))
 			*toklst = (*toklst)->next;
 		else if ((*toklst)->token->type == TOK_PIP && \
 				((*toklst)->next->token->type == TOK_RIN0 || \
