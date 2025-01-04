8d016e9fb992bfe630663d514bb9d4378b2b7079
Elpah <obengelpachris@gmail.com>
2025-01-04 04:49:08 +0100

    cleanup and fix norm errors

--- src/builtins/bltin_export.c
+++ src/builtins/bltin_export.c
@@ -6,7 +6,7 @@
 /*   By: elpah <elpah@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 09:50:30 by fmaurer           #+#    #+#             */
-/*   Updated: 2024/12/22 22:57:33 by fmaurer          ###   ########.fr       */
+/*   Updated: 2025/01/04 04:46:48 by elpah            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,18 +35,6 @@ static char	*ft_strncpy(char *dest, char *src, unsigned int n)
 		counter++;
 	}
 	return (dest);
-}
-
-char	*find_name(char *str, char *equal_pos)
-{
-	char	*result;
-
-	result = malloc(equal_pos - str + 1);
-	if (!result)
-		return (NULL);
-	ft_strncpy(result, str, equal_pos - str);
-	result[equal_pos - str] = '\0';
-	return (result);
 }
 
 // Swap Env
@@ -88,20 +76,27 @@ int	bltin_export_preout(t_cmdlst *cl, t_envlst **el)
 	char		*name;
 	char		*value;
 	char		**str;
+	int			i;
 
+	i = 1;
 	if (cl->args[1] == NULL)
 		return (0);
-	str = ft_split_input(cl->args[1]);
-	if (str && str[0])
+	check_valid_vars(cl->args);
+	while (cl->args[i] != NULL)
 	{
-		name = str[0];
-		if (str[1] && str[1][0])
-			value = str[1];
-		else
-			value = NULL;
-		set_env_entry(name, value, el);
+		str = ft_split_input(cl->args[i]);
+		if (str && str[0])
+		{
+			name = str[0];
+			if (str[1] && str[1][0])
+				value = str[1];
+			else
+				value = NULL;
+			set_env_entry(name, value, el);
+		}
+		free(str);
+		i++;
 	}
-	free(str);
 	return (0);
 }
 
