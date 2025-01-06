5f326312cf91b84aa0a14d164fc40b6b210bcabb
mofrim <mofrim@posteo.de>
2025-01-06 17:06:25 +0100

    fix memleak in get_tok_var

--- src/tokenization/tok_lvl1_get_next_var.c
+++ src/tokenization/tok_lvl1_get_next_var.c
@@ -6,7 +6,7 @@
 /*   By: fmaurer <fmaurer42@posteo.de>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/05 17:18:10 by fmaurer           #+#    #+#             */
-/*   Updated: 2024/12/30 20:41:25 by fmaurer          ###   ########.fr       */
+/*   Updated: 2025/01/06 17:04:44 by fmaurer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ static char	*get_var_value_from_env(char	*name, t_envlst *env);
 
 void	get_tok_var(t_token *tok, t_cmdline *cl, int *tok_found)
 {
+	char	*tmp;
+
 	if (!*tok_found && !cl->squot_flag && !cl->var_flag)
 	{
 		if (cl->input[cl->pos] == '$')
@@ -32,11 +34,9 @@ void	get_tok_var(t_token *tok, t_cmdline *cl, int *tok_found)
 	if (!*tok_found && cl->var_flag)
 	{
 		get_tok_word(tok, cl, tok_found);
-		if (cl->dquot_flag)
-			tok->type = TOK_WORD;
-		else
-			tok->type = TOK_WORD;
+		tmp = tok->value;
 		tok->value = get_var_value_from_env(tok->value, cl->env);
+		free(tmp);
 		cl->var_flag = 0;
 	}
 }
