fa60c4cdef3207b3a9aacc9eca4f9f9674b7d157
mofrim <mofrim@posteo.de>
2025-02-02 15:29:00 +0100

    fix: pipe parsing was completely off ?!@#%

--- src/parsing/par_parse_pipe.c
+++ src/parsing/par_parse_pipe.c
@@ -6,28 +6,30 @@
 /*   By: fmaurer <fmaurer42@posteo.de>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 11:29:40 by fmaurer           #+#    #+#             */
-/*   Updated: 2025/01/31 12:02:53 by fmaurer          ###   ########.fr       */
+/*   Updated: 2025/02/02 15:11:42 by fmaurer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-/* Parse a pipe. */
+/**
+ * Parse a pipe sybmbol.
+ *
+ * The logic here is that the TOK_PIPEs are the seperators of our cmdlst.
+ * Meaning: there will never be a TOK_PIPE that is not starting a new cmd to be
+ * parsed! Meaning: Always allocate & add_back & not forget to increment
+ * cmd_count (!!!) a new cdmdlst member with cmd set to NULL. It is that
+ * simple, bc any cases like `| echo "bla"` are already filtered out by our
+ * tokenization process.
+ */
 void	parse_pipe(t_toklst **toklst, t_cmdlst **cmd, t_cmdlst **cur_cmd,
 					int maxargs)
 {
-	if (*toklst && (*toklst)->next)
+	if (*toklst && (*toklst)->next && (*toklst)->token->type == TOK_PIP)
 	{
-		if ((*toklst)->token->type == TOK_PIP && \
-				((*toklst)->next->token->type == TOK_CMD || \
-				(*toklst)->next->token->type == TOK_BLTIN))
-			*toklst = (*toklst)->next;
-		else if ((*toklst)->token->type == TOK_PIP && \
-				!is_redir_tok((*toklst)->token->type))
-		{
-			*cur_cmd = cmdlst_new(NULL, maxargs);
-			cmdlst_add_back(cmd, *cur_cmd);
-			*toklst = (*toklst)->next;
-		}
+		*cur_cmd = cmdlst_new(NULL, maxargs);
+		cmdlst_add_back(cmd, *cur_cmd);
+		(*cmd)->cmd_count++;
+		*toklst = (*toklst)->next;
 	}
 }
